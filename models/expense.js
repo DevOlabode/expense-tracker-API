76ccda9c285804e083b3b280871db482cee02ef0
Samuel Olabode <Solabode499@gmail.com>
2025-09-10 20:26:33 -0600

    Updated app

--- models/expense.js
+++ models/expense.js
@@ -2,6 +2,10 @@ const mongoose = require('mongoose');
 const { Schema } = mongoose;
 
 const expenseSchema = new Schema({
+    name : {
+      type : String,
+      required : true
+    },
     user : {
         type : Schema.Types.ObjectId,
         ref : 'User',
--- routes/expense.js
+++ routes/expense.js
@@ -16,19 +16,44 @@ router.post('/', isLoggedIn, catchAsync(async(req, res)=>{
     res.status(200).json({msg : 'Expense Created Successfully', expense})
 }));
 
-router.get('/', catchAsync(async(req, res)=>{
+router.get('/', isLoggedIn, catchAsync(async(req, res)=>{
     const expenses  = await Expense.find({user : req.user._id});
     if(!expenses) return res.status(500).json({msg : 'Expenses cannot be fetched'})
     
     res.status(200).json({msg : 'All Expenses', expenses})    
 }));
 
-router.get('/:id', catchAsync(async(req, res)=>{
+router.get('/:id', isLoggedIn, catchAsync(async(req, res)=>{
     const expense  = await Expense.findById({_id : req.params.id, user : req.user._id});
 
     if(!expense) return res.status(404).json({msg : 'Expense Not Found'});
 
     res.status(200).json(expense)
+}));
+
+router.put('/:id', isLoggedIn, catchAsync(async(req, res)=>{
+    const expense = await Expense.findByIdAndUpdate({
+        _id : req.params.id,
+        user : req.user._id,
+    },
+    req.body,
+    {
+        new : true,
+        runValidators : true
+    }
+);
+
+    if(!expense) return res.status(404).json({msg : 'Expense Not Found'});
+
+    res.status(200).json({msg : 'Expense Edited Successfully', expense})
+}));
+
+
+router.delete('/:id', isLoggedIn, catchAsync(async(req, res)=>{
+    const expense = await Expense.findByIdAndDelete({_id : req.params.id, user : req.user._id});
+    if(!expense) return res.status(404).json({msg : 'Expense Not Found'});
+
+    res.status(200).json({msg : `The ${expense.name} was successfully deleted`})
 }));
 
 module.exports = router;
