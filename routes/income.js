286600ead940d0c3e6c2de3dd4887ff844e9ee44
Samuel Olabode <Solabode499@gmail.com>
2025-09-12 17:57:04 -0600

    Updated App

--- routes/income.js
+++ routes/income.js
@@ -23,12 +23,37 @@ router.get('/', isLoggedIn, catchAsync(async(req, res)=>{
     res.status(200).json({msg : 'All Income', income});
 }));
 
-router.put('/:id', isLoggedIn, catchAsync(async(req, res)=>{
+router.get('/:id', isLoggedIn, catchAsync(async(req, res)=>{
     const income = await Income.findOne({ _id : req.params.id, user : req.user._id});
 
-    if(!income) return res.status(404).json({msg : 'Income Not Found.'});
+    if(!income) return res.status(404).json({error : 'Income Not Found.'});
 
-    
-}))
+    res.status(200).json(income);
+}));
+
+router.put('/:id', isLoggedIn, catchAsync(async(req, res)=>{
+    const income = await Income.findOneAndUpdate({
+        _id : req.params.id,
+        user : req.user._id,
+    },
+    req.body,
+    {
+        new : true,
+        runValidators : true
+    }
+);
+    if(!income) return res.status(404).json({error : 'Income Not Found'});
+
+    res.status(200).json({msg : 'Income Updated Successfully', income})
+
+}));
+
+router.delete('/:id', isLoggedIn, catchAsync(async(req, res)=>{
+    const income = await Income.findOneAndDelete({ _id : req.params.id, user : req.user._id});
+
+    if(!income) return res.status(404).json({error : 'Income Not Found'});
+
+    res.status(200).json({msg : `The ${income.source} income was deleted`})
+}));
 
 module.exports = router;
