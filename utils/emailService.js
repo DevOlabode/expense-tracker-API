89d5c78d642fd583e130ae44a3834da984cfc811
Samuel Olabode <Solabode499@gmail.com>
2025-09-15 22:05:50 -0600

    Added Forgotten Password Feature

--- utils/emailService.js
+++ utils/emailService.js
@@ -1,8 +1,5 @@
 const nodemailer = require('nodemailer');
 
-console.log(process.env.EMAIL_PASS);
-console.log(process.env.EMAIL_USER);
-
 const transporter = nodemailer.createTransport({
   service: 'gmail',
   auth: {
